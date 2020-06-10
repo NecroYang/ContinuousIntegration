8dfb76c1c577b040d70fe25219562e5ce39c9bfa
William <necroyang@gmail.com>
2020-06-10 11:38:15 -0400

    Primera version

---
+++ src/main/java/Math.java
@@ -0,0 +1,12 @@
+
+public class Math {
+	/**
+	 * Este metodo recibe 2 numeros enteros y retorna la suma de ellos
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public static int suma(int x,int y) {
+		return x+y;
+	}
+}
---
+++ src/test/java/MathTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathTest {
+
+	@Test
+	void test() {
+		assertEquals(2, Math.suma(1, 1));
+	}
+
+}
